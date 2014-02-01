45c497a985ccc0cb6ef59a8dec8b73c828883352
Matthew Thornton <mdthorn@g.clemson.edu>
2014-02-01 18:14:54 -0500

    main.py

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
-import sys, pygame
-pygame.init()
+import pygame, sys, math, os
+from pygame.locals import *
+if not pygame.font: print('ERROR: fonts are disabled for this session')
+if not pygame.mixer: print('ERROR: sounds are disabled for this session')
 
-size = width, height = 600, 400
+class PyMain:
+    
+    def __init__(self, width=640,height=480):
+        pygame.init()
+        self.width = width
+        self.height = height
+        self.screen = pygame.display.set_mode((self.width, self.height))
 
-screen = pygame.display.set_mode(size)
 
-def main():
-
-    while True:
+class Human(object): #create the class of humans
+    def __init__(self, health, speed, weapon):
+        self.health = health
+        self.speed = speed
+        self.weapon = weapon
+if __name__ == "__main__":
+    MainWindow = PyMain()
+    MainWindow.MainLoop()
+    
+def MainLoop(self):
+    while 1:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.QUIT: 
+                sys.exit()
