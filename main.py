c4b5039a87a6bf004c9700f9e6143a9747285419
zipy124 <iiianmmm@gmail.com>
2014-02-02 22:09:19 +0000

    reverted

--- main.py
+++ main.py
@@ -13,13 +13,10 @@ class PyMain:
         self.screen = pygame.display.set_mode((self.width, self.height))
         sprite1 = sprite_class()
         back1 = sprite_class()
-        back2 = sprite_class()
         self.image = sprite1.return_sprite('player.bmp')
         self.background = back1.return_sprite("background.bmp")
-        self.background2 = back2.return_sprite("background.bmp")
         self.imgPos = sprite1.position_sprite(255,145)
         self.backPos = [0,0]
-        self.backPos2 = [0,0]
         self.fps = 30
         self.fpsTime = pygame.time.Clock()
            
