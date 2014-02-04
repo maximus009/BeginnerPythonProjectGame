5a084406e52f90beb89738b6ece1ac96d197db0a
zipy124 <iiianmmm@gmail.com>
2014-02-04 21:26:34 +0000

    button

--- main.py
+++ main.py
@@ -22,25 +22,44 @@ pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 FPS = 60
 total_frames = 0
+playing = False
 
 
 background = pygame.image.load("assets/images/background.bmp")
 PlayerYGround = SCREENHEIGHT - 166 #height from bottom according to image size in this case 166
 player = Player(0,PlayerYGround,131,166,"assets/images/player.bmp")
-
+def menu():
+    MenuImage = BaseClass(0, 0, 640, 480, "assets/images/menu.bmp")
+    Button = BaseClass(320, 240, 80, 20, "assets/images/button.bmp")
+    screen.blit(MenuImage.image, (0,0) )
+    screen.blit(Button.image, (320,240) )
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Set the x, y postions of the mouse click
+            x, y = event.pos
+            if Button.rect.collidepoint(x, y):
+                return True
+    clock.tick(FPS)
+    return False
 #-------------Main Program Loop-----------------
 while True:
-    total_frames += 1 #Putting this at the beginning so other functions can use this.
-    keystrokes(player) #Handles Key Commands and Quitting
+    if playing == True:
+        total_frames += 1 #Putting this at the beginning so other functions can use this.
+        keystrokes(player) #Handles Key Commands and Quitting
 
-    #Handles Player Movement, including jumping.
-    player.motion(SCREENWIDTH, SCREENHEIGHT)
+        #Handles Player Movement, including jumping.
+        player.motion(SCREENWIDTH, SCREENHEIGHT)
 
 
-    #DRAW
-    screen.blit(background, (0,0) )
-    BaseClass.allsprites.draw(screen) #Draws ALL sprites to the screen.
-    pygame.display.flip()
+        #DRAW
+        screen.blit(background, (0,0) )
+        BaseClass.allsprites.draw(screen) #Draws ALL sprites to the screen.
+        pygame.display.flip()
 
-    clock.tick(FPS)
+        clock.tick(FPS)
+    else:
+        playing = menu()
 
