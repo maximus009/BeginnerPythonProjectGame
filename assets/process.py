890ea9cda8bb28bdcc6cc2221af6653a55db11a3
Matthew Thornton <mdthorn@g.clemson.edu>
2014-02-04 17:51:56 -0500

    Attempt at #7

--- assets/process.py
+++ assets/process.py
@@ -39,8 +39,8 @@ def menu(BaseClass, screen, clock, FPS):
             # Set the x, y postions of the mouse click
             mouseX, mouseY = pygame.mouse.get_pos()
             if Button.rect.collidepoint(mouseX, mouseY):
-                Button2 = BaseClass(280, 240, 80, 20, "assets/images/buttonpress.bmp")
-                screen.blit(Button2.image, (280,230) )
+                Button.image = pygame.image.load("assets/images/buttonpress.bmp")
+                screen.blit(Button.image, (280,230) )
                 
                 pygame.time.delay(100)
                 return True
