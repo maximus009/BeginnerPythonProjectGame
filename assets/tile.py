b2150d5f34695ee6b21dfb09f394b86b726aba9d
Matthew Thornton <mdthorn@g.clemson.edu>
2014-02-05 02:19:23 -0500

    Added class for tiles that our character will be able to walk on.

--- assets/tile.py
+++ assets/tile.py
@@ -25,3 +25,42 @@ class Tile(pygame.Rect):
 		else:
 			print ("Tile failed to manifest onto the grid.")
 			raise ValueError #Will give an error if tile is not on the grid.
+
+	@staticmethod
+	def getTile(tileNumber):
+		#tileNumber is the key in tileDict
+		return Tile.tileDict.get(tileNumber)
+
+	@staticmethod
+	def shiftTiles(*args):
+		#Will shift tiles about our character like we had before.
+		pass
+
+
+	def __str__(self):
+		#Added this class for debugging purposes. Will print class name of tile and its coords.
+		#I may remove this later.
+		return ("{0} X: {1}, Y: {2}".format(self.__class__.__name__, self.x, self.y))
+
+class EmptyTile(Tile):
+	#Empty tiles are walkable tiles. No obstacles, in other words.
+	tileDict = {} #Dictionary that holds the tile grid.
+	tileCount = 0
+
+	def __init__(self, x, y):
+		#Calls the superclass so these tiles will be added to the list of all tiles.
+		super(EmptyTile, self).__init__(x, y)
+
+	@staticmethod
+	def getTile(tileNumber):
+		#getTile method specifically for empty tiles.
+		return EmptyTile.tileDict.get(tileNumber)
+
+	@staticmethod
+	def drawTiles(screen):
+
+		for tileNumber in xrange(EmptyTile.tileCount):
+			emmptyTile = EmptyTile.getTile(tileNumber)
+			#Draws the tile we just made!
+			#Replace BLACK with image later.
+			pygame.draw.rect(screen, BLACK, emmptyTile)
