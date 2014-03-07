606c850083a432b22c4df44e0295e1424f567099
ashdude1120 <ashlyn.duy@gmail.com>
2014-03-07 07:45:24 -0600

    Update classes.py
    Added a pause class-Now available for intergration. It also supports a pause sound which I can, or anybody else, add.

--- assets/classes.py
+++ assets/classes.py
@@ -16,6 +16,37 @@ class BaseClass(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
         self.width, self.height = width, height
+
+class PauseScene:
+	def __init__(self, bg):
+		self.bg = bg
+		self.next = self
+		self.flags = ''
+		self.counter = 0
+	
+	def processInput(self, events, pressed):
+		for event in events:
+			if event.down and event.action == 'start':
+				if 'M' in self.bg.flags:
+					playNoise('pause_sound')
+					pygame.mixer.music.unpause()
+				self.next = self.bg
+				self.bg.next = self.bg
+	
+	def update(self):
+		if 'M' in self.bg.flags:
+			if self.counter == 0:
+				pygame.mixer.music.pause()
+				playNoise('pause_sound')
+		
+		self.counter += 1
+	
+	def render(self, screen, rc):
+		screen.fill(BLACK)
+		img = getText(WHITE, "PAUSE")
+		x = 128 - img.get_width() // 2
+		y = 112 - img.get_height() // 2
+		screen.blit(img, (x, y))
         
 class Player(BaseClass):
 
